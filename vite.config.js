4fc37654b182430a2ba3bef5c940375ab4622b79
Emiliano Pignatelli <emipignatelli@hotmail.com>
2025-02-06 20:42:51 -0300

    Solucionando compilaciones

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteImagemin from 'vite-plugin-imagemin'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImagemin({
+      gifsicle: {
+        optimizationLevel: 3,
+        interlaced: false
+      },
+      optipng: {
+        optimizationLevel: 7
+      },
+      mozjpeg: {
+        quality: 75
+      },
+      pngquant: {
+        quality: [0.6, 0.8],
+        speed: 4
+      }
+    })
+  ],
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
 })
